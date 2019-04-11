ea3fe583775eeb2f6ac50f83dac2a6954f3dfaae
82105 <thrilcho@naver.com>
2019-04-11 13:22:58 +0900

    github 마스터연지

---
+++ 실험용프로젝트/src/com/happy/MainPractice.java
@@ -0,0 +1,19 @@
+package com.happy;
+
+public class MainPractice {
+
+	public static void main(String[] args) {
+		String name = "gsda";
+		String [] names = {"kim","lee","park"};
+		
+		for (int i = 0; i < names.length; i++) {
+			String name2 = names[i];
+			if (name2== "kim") {
+				System.out.println(name2);
+			}
+		}
+		
+	
+	}
+
+}
